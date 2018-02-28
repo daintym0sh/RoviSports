b31203f5ea28467df15315d1084d1759591b61a7
John Meier <john.meier@rovisys.com>
2018-02-28 14:25:42 -0500

    changes for heroku deployment

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+
 //Install express server
 const express = require('express');
 const app = express();
+const path = require('path');
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
@@ -12,3 +14,4 @@ app.listen(process.env.PORT || 8080);
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
+
