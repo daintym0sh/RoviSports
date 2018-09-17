b5a28a239a0661e64d16cecdc1692ed23803f643
John Meier <john.meier@rovisys.com>
2018-09-17 14:17:59 -0400

    Changed path location strategy in server.js

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.listen(process.env.PORT || 8080);
 
 //Path location strategy
 app.get('*', function(req,res) {
-  res.sendFile(path.join(__dirname+'./dist/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
