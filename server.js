caf2baf2aa64fa761f50091d0c5220cb0d74e395
John Meier <john.meier@rovisys.com>
2018-09-17 14:31:05 -0400

    Changed path location strategy in server.js to include app name

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const app = express();
 const path = require('path');
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/bing-bong'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
 //Path location strategy
 app.get('*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/bing-bong/index.html'));
 });
